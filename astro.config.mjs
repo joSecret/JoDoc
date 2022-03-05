2724421f0bed84644aa993fa0f4d72067caccd07
Ion Obreja <jo@da.md>
2022-03-06 00:12:09 +0200

    Image fixed issus

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,14 @@ export default {
   // projectRoot: '.',     // Where to resolve all URLs relative to. Useful if you have a monorepo project.
   // pages: './src/pages', // Path to Astro components, pages, and data
   dist: './docs',
-  // public: './public',   // A folder of static files Astro will copy to the root. Useful for favicons, images, and other files that don’t need processing.
+  public: './public',
   buildOptions: {
     site: 'https://josecret.github.io/JoDoc/',
-    sitemap: true,         // Generate sitemap (set to "false" to disable)
+    sitemap: true,
   },
   devOptions: {
-    // hostname: 'localhost',  // The hostname to run the dev server on.
-    // port: 3000,             // The port to run the dev server on.
+    // hostname: 'jodoc',
+    // port: 3000,
   },
   renderers: [
     "@astrojs/renderer-preact",
